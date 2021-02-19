da707a614b5b6098e7586f60876208982a93433e
Ertugrul Eryildiz <ertugruleryldz@gmail.com>
2021-02-19 14:29:21 -0500

    This commit will add tabView to Flix app where user gets to see superhero movies.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ertugrul Eryildiz on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
